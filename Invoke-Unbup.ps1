402018c5fb1a8d45c3b2fcc1f835df9922ce4a37
WiredPulse <WiredPulse@users.noreply.github.com>
2019-08-25 18:23:29 -0400

    Update Invoke-Unbup.ps1

--- Invoke-Unbup.ps1
+++ Invoke-Unbup.ps1
@@ -1,8 +1,9 @@
 ﻿<#
 .SYNOPSIS  
-    When McAfee quarantines a binary, it XORs the item, renames the item with random characters, changes the extension to .bup, and typically stores the item in C:\Quarantine. In the 
-    end, this all makes the file unusable on the system. This script reverses that process using 7-zip, the publicly known key, and then zips it with a user-defined password. If no 
-    password is provided, the default password of "!nf3ct3d!". This script ultimately restores the binary into its orginal state. 
+    When McAfee quarantines a binary, it XORs and renames the item with random characters, changes the extension to .bup, and
+    typically stores the item in C:\Quarantine. In the end, this all makes the file unusable on the system. This script 
+    reverses that process using 7-zip, the publicly known key, and then zips it with a user-defined password. If no password 
+    is provided, the default password of "!nf3ct3d!" is used. This script ultimately restores the binary into its orginal state.
 
 .PARAMETER path 
     Used to specify the path to the .bup files. The .bup files should be in a directory by themselves.
